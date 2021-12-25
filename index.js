f6fe126afca88936ff760eafffa87423cee6f6ec
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2021-12-25 15:17:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,3 @@ const app = express();
 app.use('/', require('./controllers/controller'));
 
 app.listen(3000);
-
-// const mongoose = require('mongoose');
-
-// main().catch(err => console.log(err));
-
-// async function main() {
-//     await mongoose.connect('mongodb+srv://bot:iGbLGncZJIEXeKRI@cluster0.1ipw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-// }
-
-// const schema = new mongoose.Schema({ name: 'string', size: 'string' });
-// const Tank = mongoose.model('Tank', schema, 'toto');
-
-// Tank.watch().on('change', data => {
-//     console.log(data);
-// })
-
-// // const small = new Tank({ size: 'small' });
-// // small.save(function (err) {
-// //     if (err) throw err;
-// //     console.log("inserted");
-// // });
-
-// // or
-
-// // Tank.create({ size: 'small' }, (err, doc) => {
-// //     if (err) throw err;
-// //     console.log(doc);
-// // });
