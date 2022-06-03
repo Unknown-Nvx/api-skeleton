55a4f076a1288d388cbfe742b6cbf5d894c2e21a
Erwann Neveux <67058378+Unknown-Nvx@users.noreply.github.com>
2022-06-03 14:51:55 +0200

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,30 +3,15 @@ console.log("controller loaded");
 const express = require('express');
 const router = express.Router();
 
-const keys = require('../models/Keys');
-const Keys = new keys();
-
-const apiKeyAuth = (req, res, next) => {  // middleware to check if the request have an API Key
-    let keysList = []; //tmp
-    const key = req.query.id;
-    const keyIsValid = keysList.includes(key);
-
-    if (!keyIsValid) {
-        res.json({ error: "API Key does not exist or is incorrect" });
-        return;
-    }
-    next();
-}
-
-router.get('/', apiKeyAuth, (req, res, next) => {
+router.get('/', (req, res, next) => {
     require('../routes/home')(req, res);
 })
 
-router.get('/second', apiKeyAuth, (req, res, next) => {
+router.get('/second', (req, res, next) => {
     res.json({ message: "second page" });
 })
 
-router.get('/third', apiKeyAuth, (req, res, next) => {
+router.get('/third', (req, res, next) => {
     res.json({ message: "third page" });
 })
 
